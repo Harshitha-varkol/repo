18bc76645d12ae4c03737befb1729ec4aa8f847b
lovely <lillyflow729@gmail.com>
2025-12-09 20:37:59 +0530

    resume

---
+++ resume/my_resume/views.py
@@ -0,0 +1,103 @@
+from django.shortcuts import render,HttpResponse
+
+# Create your views here.
+
+
+def resume(request):
+    return HttpResponse(''' 
+<div style="width:1000px;position: relative;left:150px;background-color: white">
+<div style="font-size:25px;text-align:center;font-weight:600">VORKOLU HARSHITHA</div>
+<div style="font-size:20px;text-align:center">varkolharshitha@gmail.com —  9398320990</div>                        
+<div style="font-size: 25px;font-weight: 700;">Professional Summary</div>
+<hr style="border:1px solid">
+<div style="font-size: 18px;margin-left:20px">A dedicated and passionate Data Science graduate currently pursuing a Master of Computer Applications (MCA) at Aurora <br>
+PG College, Nampally. Skilled in Python, Machine Learning, and SQL, with hands-on experience in developing predictive <br>
+models and fraud detection systems. Seeking an entry-level role to apply technical and analytical skills while continuing to <br>
+grow professionally.
+</div>
+<br>
+<div style="font-size: 25px;font-weight: 700;">Education</div>
+<hr style="border:1px solid">
+<div style="font-size: 21px ; font-weight: 800">Aurora PG College, Nampally</div>
+<div style="font-size: 18px;margin-left:20px">
+    <span>
+    Master of Computer Applications(MCA) 2023-2035
+    </span>
+</div>
+<div style="font-size: 18px;margin-left:20px">GPA: 8.3/10</div>
+<br>
+<div style="font-size: 21px ; font-weight: 800;">Sree Chaitanya Degree College, Karimnagar
+</div>
+<div style="display:flex;gap:60%;font-size: 18px;">
+    <span style="margin-left:20px">
+    Bachelor of Science in Data Science 2020-2023
+    </span>
+</div>
+<div style="font-size: 18px;margin-left:20px">GPA: 7.5/10</div>
+<br>
+<div style="font-size: 25px; font-weight: 700;">Technical Skills</div>
+<hr style="border:1px solid">
+<div style="margin-left:20px"><span style="font-size:20px;font-weight: 500;"margin-left:20px>• Programming:</span> <span style="font-size: 18px;">>Python, SQL</span> </div> <br>
+<div style="margin-left:20px"><span style="font-size:20px;font-weight: 500;"margin-left:20px>• Machine Learning:</span> <span style="font-size: 18px;"Python, SQL>Scikit-learn, TensorFlow</span> </div><br>
+<div style="margin-left:20px"><span style="font-size:20px;font-weight: 500;"margin-left:20px>• Data Analysis:</span> <span style="font-size: 18px;"Python, SQL>Pandas, NumPy</span> </div><br>
+<div style="margin-left:20px"><span style="font-size:20px;font-weight: 500;"margin-left:20px>• Data Visualization:</span> <span style="font-size: 18px;"Python, SQL>Matplotlib, Sea</span> </div>
+<br>
+<div style="font-size: 25px;font-weight: 700;">Projects</div>
+<hr style="border:1px solid">
+<div style="font-size: 20px;font-weight: 600;">YouTube Ad Views Prediction</div>
+<div style="font-size: 18px;font-weight: 400;margin-left:20px">- Developed a predictive model using Scikit-learn and TensorFlow <br>
+    - Improved accuracy by 15% through feature optimization
+</div>
+<div style="font-size: 20px;font-weight: 600;">Fraud Detection in E-Commerce Transactions</div>
+<div style="font-size: 18px;font-weight: 400;margin-left:20px">.Built a fraud detection system using Scikit-learn and TensorFlow. <br>
+- Enhanced real-time detection accuracy by 20%</div>
+<br>
+<div style="font-size: 25px;font-weight: 700;">Achievements</div>
+<hr style="border:1px solid">
+<span style="font-size:20px;font-weight: 550;margin-left:20px">• NSIT (National Science Indian Talent):</span>  <span style="font-size: 18px">Awarded for excellence in science and technology</span> <br>
+<span style="font-size:20px;font-weight: 550;margin-left:20px">• Certificate of Media and Entertainment (2018): </span>  <span style="font-size:18px"> Trained in Animation for 2 years and secured 98%.</span>
+<br><br>
+<div style="font-size: 25px;font-weight: 700;" >
+    Certifications
+</div>
+<hr style="border:1px solid">
+<div style="font-size: 18px;font-weight: 400;margin-left:20px">• Machine Learning with Python - Internship Studio</div>
+ <div style="font-size: 18px;font-weight: 400;margin-left:20px">• Data Science and Analytics - Manac@Infotec</div>
+ <br>
+<div style="font-size: 25px;font-weight: 700;">
+    Soft Skills
+</div>
+<hr style="border:1px solid">
+<div style="font-size: 18px;font-weight: 400;margin-left:20px">• Effective Communication & Teamwork</div>
+<div style="font-size: 18px;font-weight: 400;margin-left:20px">• Time Management & Adaptability</div>
+<div style="font-size: 18px;font-weight: 400;margin-left:20px">• Problem-Solving & Analytical Thinking</div>
+<div style="font-size: 18px;font-weight: 400;margin-left:20px">- Built a fraud detection system using Scikit-learn and TensorFlow.</div>
+<br>
+<div style="font-weight: 700;font-size: 25px;"> 
+    Interests
+</div>
+
+<hr style="border:1px solid">
+<div style="font-size: 18px;font-weight: 400;margin-left:20px">Data Science, Artificial Intelligence, Chess</div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+                        
+                        
+                        
+                        
+                            
+                                                                                                                                    
+                                               
+
+''')
